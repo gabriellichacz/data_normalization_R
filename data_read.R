35a896d6d19fb04bb3fb9ec67af3a641d8a31914
gabriellichacz <gabriellichacz@gmail.com>
2022-12-09 13:44:58 +0100

    read data

---
+++ data_read.R
@@ -0,0 +1,9 @@
+######################## init ########################
+setwd("#")
+
+######################## data ########################
+
+data1 <- read.csv("Airfields.csv")
+data2 <- read.csv("Airlines.csv")
+data3 <- read.csv("MainDataFlights.csv")
+data4 <- read.csv("States.csv")
